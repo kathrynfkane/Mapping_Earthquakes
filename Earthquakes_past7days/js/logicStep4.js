88ca4e0537ff5f391ab02f1ebd1add6b76da9232
Kathryn Kane <Kathryn@Kathryns-MacBook-Pro-2.local>
2021-04-04 15:18:39 -0700

    deliverable 1

--- Earthquakes_past7days/js/logicStep4.js
+++ Earthquakes_past7days/js/logicStep4.js
@@ -16,12 +16,6 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
     id: "mapbox/outdoors-v11",
     accessToken: API_KEY
   });
-  // Create the map object with center at the San Francisco airport.
-let map = L.map('mapid', {
-  center: [39.5, -98.5],
-  zoom: 3,
-  layers: [streets]
-});
 
 // Create a base layer that holds both maps.
 let baseMaps = {
@@ -29,19 +23,12 @@ let baseMaps = {
   "Satellite": satelliteStreets
 };
 
-// Create the earthquake layer for our map.
-let earthquakes = new L.layerGroup();
-// We define an object that contains the overlays.
-// This overlay will be visible all the time.
-let overlays = {
-  Earthquakes: earthquakes
-};
-
-// Then we add a control to the map that will allow the user to change
-// which layers are visible.
-L.control.layers(baseMaps, overlays).addTo(map);
-
-
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid', {
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
+});
 
 L.control.layers(baseMaps).addTo(map);
 
@@ -59,8 +46,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
           onEachFeature: function(feature, layer) {
             layer.bindPopup("Magnitude:" + feature.properties.mag + "<br>Location:" + feature.properties.place);
           }
-          }).addTo(earthquakes);
-          earthquakes.addTo(map);
+          }).addTo(map);
   
 });
  // This function returns the style data for each of the earthquakes we plot on
